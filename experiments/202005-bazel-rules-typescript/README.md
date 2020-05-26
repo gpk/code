c682f43bcad5ea5c6a9b575380e833f0508f3e61
Steve Conover <sconover@gmail.com>
2020-05-26 07:57:04 +0200

    bazel rules nodejs initial

---
+++ experiments/202005-bazel-rules-typescript/README.md
@@ -0,0 +1,3 @@
+Vanilla rules_typescript demonstration.
+
+Following instructions: https://bazelbuild.github.io/rules_nodejs/install.html
---
+++ experiments/202005-bazel-rules-typescript/truck/src/truck.ts
@@ -0,0 +1,10 @@
+import "../../shipping-container-types/types"
+
+const x = "hello world 2"
+
+const y: shippingContainer.Container = {
+    weight: 50000,
+    company: "COSCO",
+    size: shippingContainer.ContainerSize.FULL
+}
+

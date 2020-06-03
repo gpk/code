4a719f258fdf05b876fe271a1548f270f926b97d
Steve Conover <sconover@gmail.com>
2020-06-03 09:57:51 +0200

    Load project python files into pyodide
    - The build codegen's a typescript file, managed-by-build.ts,
      which is generated from a python script.
    - The generated ts file contains a mapping of all
      project python paths, to the corresponding code from
      each file.
    - Using the emscripten file access api, load each of these
      files into pyodide at page load time
    - Demonstrate end-to-end by calling a function from
      a project python file, from pyodide.
    
    two production py files
    system for generating, compiling, and bundling managed-by-build.ts
    load all non-test py files into PROJECT_PYTHON_FILES typescript constant
    demonstration of loading python project files into pyodide and calling a function

---
+++ script/insert_managed_by_build_ts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+this_dir=$(dirname $0)
+
+codegen_init_dir=$this_dir/../src/ts/build/codegen/init
+$this_dir/compose_managed_by_build_ts_source_file $this_dir/../src/py > /tmp/managed-by-build.ts.new
+
+should_compile=yes
+
+if [ -f $codegen_init_dir/managed-by-build.ts ]; then
+    set +e
+    diff $codegen_init_dir/managed-by-build.ts /tmp/managed-by-build.ts.new
+    if [ "0" = "$?" ]; then
+        echo "managed-by-build.ts matches, skip compile"
+        should_compile=no
+    fi
+    set -e
+fi
+
+mkdir -p $codegen_init_dir
+echo '{"extends": "../../../tsconfig-core.json"}' > $codegen_init_dir/tsconfig.json
+mv /tmp/managed-by-build.ts.new $codegen_init_dir/managed-by-build.ts
+
+if [ $should_compile = "yes" ]; then
+    time $this_dir/../src/ts/node_modules/.bin/tsc --incremental -p $codegen_init_dir/tsconfig.json
+    cp $codegen_init_dir/managed-by-build.* $this_dir/../src/ts/build/tsc/init/src/
+fi
---
+++ src/ts/init/src/managed-by-build.ts
@@ -0,0 +1,5 @@
+// The build injects a json map here, which is a map of
+// containing python file, to content, of all "production" python code in the project
+
+export const PROJECT_PYTHON_FILES: { [key: string]: string } = {}
+

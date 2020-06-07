c6131843afb01394e8a4ce59d3a407dd475c4ea1
Steve Conover <sconover@gmail.com>
2020-06-07 19:35:16 +0200

    Send slack notification when build completes
    Indicate devsite url, build url, build outcome, deploy outcome

--- script/gh-action-master.sh
+++ script/gh-action-master.sh
@@ -3,12 +3,27 @@
 this_dir=$(dirname $0)
 PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 
+start_time_s=$(date +%s)
+build_outcome=fail
+deploy_outcome=nodeploy
+
+function send_chat {
+    time_to_execute=$(expr $(date +%s) - $start_time_s)
+
+    DEV_URL=http://dev.gpkpw.com/offmaster/$GITHUB_REF \
+        GITHUB_ACTION_URL=https://github.com/gpk/program-world/actions/runs/$GITHUB_RUN_ID \
+        BUILD_SERIES=master \
+        time $this_dir/send_chat.sh $time_to_execute $build_outcome $deploy_outcome
+}
+trap send_chat EXIT
+
 time $this_dir/init_ts.sh
 time $this_dir/init_py.sh
 time $this_dir/build.sh
 time $this_dir/test_py.sh
 time $this_dir/test_ts.sh
 time $this_dir/typecheck_py.sh
+build_outcome=ok
 
 if [ ! -z "$GITHUB_REF" ]; then
     mkdir ~/secrets
@@ -20,3 +35,4 @@ else
 fi
 
 time $this_dir/deploy_to_firebase.sh prod
+deploy_outcome=deployed
---
+++ script/send-slack-notification.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const assert = require('assert')
+const IncomingWebhook = require('@slack/webhook').IncomingWebhook
+
+assert.ok(process.env.SLACK_WEBHOOK_URL != null)
+assert.ok(process.env.GITHUB_ACTION_URL != null)
+assert.ok(process.env.GITHUB_ACTOR != null)
+assert.ok(process.env.GITHUB_REF != null)
+assert.ok(process.env.BUILD_SERIES != null)
+
+const args = process.argv.slice(2)
+assert.equal(args.length, 4)
+const slackJsonPath = args[0]
+const secondsToExecute = parseInt(args[1])
+const buildStatus = args[2]
+const deployOk = args[3] === "deployed"
+const shortBranchName = process.env.GITHUB_REF.substring(process.env.GITHUB_REF.lastIndexOf("/") + 1)
+
+const githubActorToSlackUserId = JSON.parse(fs.readFileSync(slackJsonPath))
+
+const buildSeriesEmoji = process.env.BUILD_SERIES == "master" ? ":tophat:" : ":rainbow:"
+
+let buildStatusEmoji = null
+if (buildStatus === "ok") {
+  buildStatusEmoji = ":white_check_mark:"
+} else if (buildStatus === "fail") {
+  buildStatusEmoji = ":x:"
+} else {
+  buildStatusEmoji = ":barber:"
+}
+assert.ok(buildSeriesEmoji != null)
+
+let message = "" + secondsToExecute + "s "
+message += buildSeriesEmoji
+message += " "
+message += buildStatusEmoji
+
+if (deployOk) {
+  assert.ok(process.env.DEV_URL != null)
+  message += ` <${process.env.DEV_URL}|[devsite]>`
+}
+
+message += ` <${process.env.GITHUB_ACTION_URL}|[build]>`
+message += ` branch: ${shortBranchName}`
+message += ` <@${githubActorToSlackUserId[process.env.GITHUB_ACTOR]}> `
+
+new IncomingWebhook(process.env.SLACK_WEBHOOK_URL).send(message)

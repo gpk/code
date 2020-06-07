bbe03a502f7ef819c2c4c186a588d8cfde6bc686
Steve Conover <sconover@gmail.com>
2020-06-07 10:52:01 +0200

    Deploy artifacts to dev.gpkpw.com, under a folder for the branch
    Every successful build (both master and off-master builds)
    results in an upload of build artifacts to a gcs bucket that's
    an http static website, to a subfolder on that site that's
    named after the git branch.
    
    This allows us to easily share off-master work with each
    other.
    
    basic script that uploads all files in a dir to gcs
    integration deploy to gcs into the build

--- script/gh-action-off-master.sh
+++ script/gh-action-off-master.sh
@@ -6,4 +6,14 @@ PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 time $this_dir/init_ts.sh
 time $this_dir/init_py.sh
 time $this_dir/build.sh
-time $this_dir/deploy.sh
+
+if [ ! -z "$GITHUB_REF" ]; then
+    mkdir ~/secrets
+    echo "$PW_DEV_UPLOAD_GOOGLE_APPLICATION_CREDENTIALS" > ~/secrets/PW_DEV_UPLOAD_GOOGLE_APPLICATION_CREDENTIALS
+    export GOOGLE_APPLICATION_CREDENTIALS=~/secrets/PW_DEV_UPLOAD_GOOGLE_APPLICATION_CREDENTIALS
+    time $this_dir/deploy_to_gcs.sh offmaster/$GITHUB_REF
+else
+    echo "GITHUB_REF unset, will not deploy to gcs"
+fi
+
+time $this_dir/deploy_to_firebase.sh
--- script/test_ts.sh
+++ script/test_ts.sh
@@ -6,4 +6,4 @@ PATH="/usr/bin:/bin:/usr/sbin:/sbin:$($this_dir/../bin/realpath $this_dir/../bin
 cd $this_dir/..
 
 export NODE_PATH=src/ts/node_modules
-exec node src/custom-mocha/bin/_mocha --srcDir=src/ts --tscOutDir=src/ts/build/tsc --preRun=script/tsc_build.sh src/ts/**/*-test.ts
+exec node src/ts/custom-mocha/bin/_mocha --srcDir=src/ts --tscOutDir=src/ts/build/tsc --preRun=script/tsc_build.sh src/ts/**/*-test.ts

7c140fb086ab23796dad447c65d2831c1a7e58a5
Steve Conover <sconover@gmail.com>
2020-06-10 18:51:33 +0200

    Deploy artifacts to dev.gpkpw.com, under a folder for the branch (#16)
    Every successful build (both master and off-master builds)
    results in an upload of build artifacts to a gcs bucket that's
    an http static website, to a subfolder on that site that's
    named after the git branch.
    
    This allows us to easily share off-master work with each
    other.
    
    basic script that uploads all files in a dir to gcs
    integration deploy to gcs into the build

--- script/init_ts.sh
+++ script/init_ts.sh
@@ -6,11 +6,28 @@ PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 bin_dir=$(dirname $0)/../bin
 
 test -d $bin_dir || mkdir $bin_dir
+
+
+# not really ts-related...
+if [ $(uname) = "Darwin" ]; then
+    if [ ! -f /usr/local/bin/realpath ]; then
+        brew install coreutils
+    fi
+    ln -sf /usr/local/bin/realpath $bin_dir/realpath
+fi
+
+if [ $(uname) = "Linux" ]; then
+    ln -sf /usr/bin/realpath $bin_dir/realpath
+fi
+
+
+
 ln -sf /usr/local/bin/node $bin_dir/node
 ln -sf /usr/local/bin/npm $bin_dir/npm
 
 # in the build, the cache is keyed on a hash of package-lock.json
 if [ ! -d "$this_dir/../src/ts/node_modules" -o $(uname) = "Darwin" ]; then
+    PATH="/usr/bin:/bin:/usr/sbin:/sbin:$($this_dir/../bin/realpath $this_dir/../bin)"
     cd $this_dir/../src/ts
     time ../../bin/npm install
 fi
